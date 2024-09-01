e7d6b79f293c12216a7c94e9b179f9ec1693c3f9
APD <153990827+APD-Kim@users.noreply.github.com>
2024-09-01 23:10:23 +0900

    ✨ [Add]: scrap feature

--- src/scraps/scraps.controller.ts
+++ src/scraps/scraps.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { ScrapService } from './scraps.service';
 
-@Controller('scrap')
-export class ScrapController {}
+const userId = 1;
+
+@Controller('scraps')
+export class ScrapController {
+  constructor(private readonly scrapsService: ScrapService) {}
+  // 게시물 스크랩
+  // @UseGuards(JwtGuard)
+  @Post('/:postId')
+  async scrapPost(@Param('postId') postId: number) {
+    const result = await this.scrapsService.scrapPost(postId, userId);
+    return result;
+  }
+  //내가 스크랩한 게시물 조회
+  @Get()
+  async getScrapPosts() {
+    const result = await this.scrapsService.getScrapPosts(userId);
+    return result;
+  }
+  //스크랩한 게시물 삭제
+  // @UseGuards(JwtGuard)
+  @Delete('/:scrapId')
+  async deleteScrapPost(@Param('scrapId') scrapId: number) {
+    const result = await this.scrapsService.deleteScrapPost(scrapId, userId);
+    return result;
+  }
+}
--- src/scraps/scraps.service.ts
+++ src/scraps/scraps.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PostsEntity } from '../posts/entities/base-posts.entity';
+import { Repository } from 'typeorm';
+import { ScrapsEntity } from './entities/scraps.entity';
 
 @Injectable()
-export class ScrapService {}
+export class ScrapService {
+  @InjectRepository(PostsEntity)
+  private postRepository: Repository<PostsEntity>;
+  @InjectRepository(ScrapsEntity)
+  private scrapRepository: Repository<ScrapsEntity>;
+
+  async scrapPost(postId: number, userId: number) {
+    const post = await this.postRepository.findOneBy({ postId });
+    if (!post) throw new NotFoundException(`${postId}번 게시글을 찾을 수 없습니다`);
+
+    const scrapedPost = this.scrapRepository.create({ userId, postId });
+
+    const result = await this.scrapRepository.save(scrapedPost);
+
+    return result;
+  }
+
+  async getScrapPosts(userId: number) {
+    const result = await this.scrapRepository.find({ where: { userId } });
+    return result;
+  }
+  async deleteScrapPost(scrapId: number, userId: number) {
+    const scrapPost = await this.scrapRepository.findOneBy({ scrapId });
+
+    if (!scrapPost) throw new NotFoundException(`스크랩한 게시물을 찾을 수 없습니다.`);
+    if (scrapPost.userId !== userId) throw new ForbiddenException(`삭제한 권한이 없습니다.`);
+    const result = await this.scrapRepository.softDelete({ scrapId });
+
+    return result;
+  }
+}

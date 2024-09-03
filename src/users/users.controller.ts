945d09c682799dcca133e4c82fc76c8dfefa190f
devellybutton <leegr0106@gmail.com>
2024-09-03 12:21:41 +0900

    [✨feat]: users controller 파일 세팅

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Patch } from '@nestjs/common';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('me')
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
+    // 본인 정보 조회
+    @Get()
+    async getMyInfo(){}
+
+    // 본인 정보 수정
+    @Patch()
+    async patchMyInfo(){}
+
+    // 본인 게시글 전체 조회
+    @Get('/posts')
+    async getMyPosts(){}
+
+    // 본인 댓글 전체 조회
+    @Get('/comments')
+    async getMyComments(){}
+
+    // 본인 스크랩 게시물 조회
+    @Get('/scraps')
+    async getMyScraps(){}
+}

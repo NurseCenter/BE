1f2492aa04dabe64b5c00d422e27a467a12a0d20
devellybutton <leegr0106@gmail.com>
2024-09-24 18:57:20 +0900

    [✨feat]: 정지회원 판별하는 guard 추가

---
+++ src/auth/guards/suspension.guard.ts
@@ -0,0 +1,24 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersDAO } from 'src/users/users.dao';
+
+@Injectable()
+export class SuspensionGuard implements CanActivate {
+  constructor(private readonly usersDAO: UsersDAO) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const userId = request.user?.userId;
+
+    if (!userId) {
+      throw new UnauthorizedException('로그인된 사용자가 아닙니다.');
+    }
+
+    const user = await this.usersDAO.findUserByUserId(userId); 
+    if (user && user.suspensionEndDate && user.suspensionEndDate > new Date()) {
+        const daysRemaining = Math.ceil((user.suspensionEndDate.getTime() - new Date().getTime()) / (1000 * 3600 * 24));
+        throw new UnauthorizedException(`현재 계정이 정지 상태입니다. ${daysRemaining}일 후에 해제됩니다. 마이페이지에서 확인해 주세요.`);
+      }
+  
+    return true;
+  }
+}

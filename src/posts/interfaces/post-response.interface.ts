8c978737f1d0d061f0541b400bd3c7e3342d3ad9
devellybutton <leegr0106@gmail.com>
2024-10-02 20:56:25 +0900

    [♻️refactor]: presignedPost data는 옵셔널 타입으로 변경

--- src/posts/interfaces/post-response.interface.ts
+++ src/posts/interfaces/post-response.interface.ts
@@ -5,6 +5,6 @@ export interface IPostResponse {
   content: string; // 내용 (요약본)
   hospitalNames?: string[]; // 게시물과 관련된 병원 이름 (배열)
   createdAt: Date; // 작성일
-  updatedAt?: Date; // 수정일 (업데이트 유무 렌더링, 수정된 경우에만 존재)
-  presignedPostData: string[]; // presigned URL
+  updatedAt?: Date; // 수정일 (업데이트 유무 렌더링 목적)
+  presignedPostData?: string[]; // presigned URL
 }

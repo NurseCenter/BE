d51100f32b9eae37c6bc08d6e03941435ebb078b
devellybutton <leegr0106@gmail.com>
2024-10-13 08:33:39 +0900

    [📝docs]: 소켓 관련 API 스웨거 설명 추가

--- src/session/session.controller.ts
+++ src/session/session.controller.ts
@@ -2,11 +2,18 @@ import { Controller, Post, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { SessionService } from './session.service';
 import { extractSessionIdFromCookie } from 'src/common/utils';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Session')
 @Controller('session')
 export class SessionController {
   constructor(private readonly sessionService: SessionService) {}
 
+  @Post('monitor')
+  @ApiOperation({ summary: '세션 만료 알림 발송을 위한 모니터링 시작 (Request 헤더의 쿠키에서 세션 ID를 추출함)' })
+  @ApiResponse({ status: 200, description: '세션 모니터링이 시작되었습니다.', schema: { example: { message: '세션 모니터링이 시작되었습니다.' } } })
+  @ApiResponse({ status: 400, description: '세션 ID를 찾을 수 없습니다.', schema: { example: { error: '세션 ID를 찾을 수 없습니다.' } } })
+  @ApiResponse({ status: 500, description: '세션 모니터링 중 오류가 발생했습니다.', schema: { example: { error: '세션 모니터링 중 오류가 발생했습니다.' } } })
   @Post('monitor')
   async monitorSession(@Req() req: Request, @Res() res: Response): Promise<void> {
     try {
@@ -36,6 +43,9 @@ export class SessionController {
   }
 
   @Post('extend')
+  @ApiOperation({ summary: '세션 연장 (Request 헤더의 쿠키에서 세션 ID를 추출함)' })
+  @ApiResponse({ status: 200, description: '세션이 갱신되었습니다.', schema: { example: { message: '세션이 갱신되었습니다.' } } })
+  @ApiResponse({ status: 500, description: '세션 갱신 중 오류가 발생했습니다.', schema: { example: { error: '세션 갱신 중 오류가 발생했습니다.' } } })
   async extendSession(@Req() req: Request, @Res() res: Response): Promise<void> {
     try {
       await this.sessionService.extendSession(req);
